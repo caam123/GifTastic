ff43fa2f077e129953af4aaa4c2d7eaf1c141bc3
Charlie <caam_1275@hotmail.com>
2019-04-05 02:00:46 -0600

    Desplegar un solo (UNO) con rating + botones, probar en loop despues

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,7 +36,10 @@ $("#addBand").on("click", function(event){
 });
 
 // ===== Comentario importante!!! dentro de .on ( ".botones") ayuda a descender del id de botones list y filtrar a través de la clase, por alguna razon no podia hacerlo de la forma de siempre ===== ¿? //
+
+
 $("#botonesList").on("click", ".botones", function(){
+   $("#GifSection").empty();
     
     var inputBand = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ inputBand+ "&api_key=" + "I2RjtQO6f9dyGFX4Yt6r1GKXbWD0gU1w&limit=5"
@@ -51,10 +54,50 @@ $("#botonesList").on("click", ".botones", function(){
         var imgUrl = response.data[0].images.fixed_height_still.url;
         console.log(imgUrl);
 
+        var gifContainer = $("<div>");
+            gifContainer.addClass("gifContainer")
+
+    
+        var gifThumb = $("<div>");
+            gifThumb.addClass("gifThumb");
+
         var bandGif = $("<img>");
             bandGif.attr("src", imgUrl);
+
+        var gifButtons = $("<div>");
+            gifButtons.addClass("gifButtons");
+        var pButtons = $("<p>");
+            pButtons.addClass("rating");
+            pButtons.text("Rating es____");
         
-        $("#gifThumb").append(bandGif);
+        var buttonSave = $("<button>");
+            buttonSave.addClass("saveFav save");
+
+         var buttonFave = $("<button>");
+            buttonFave.addClass("saveFav fav");   
+            
+        var saveIcon = $("<i>");
+            saveIcon.addClass("far fa-save");
+        
+        var heartIcon = $("<i>");
+            heartIcon.addClass("far fa-heart");
+
+        buttonSave.append(saveIcon);
+        buttonFave.append(heartIcon);
+ 
+        gifButtons.append(pButtons);
+        gifButtons.append(buttonSave);
+        gifButtons.append(buttonFave);
+
+        $("#GifSection").append(gifContainer);     
+        $(".gifContainer").append(gifThumb);
+        $(".gifContainer").append(gifButtons);
+        $(".gifThumb").append(bandGif);
+
+
+        /*$(".gifContainer").append(pButtons);
+        $(".gifContainer").append(buttonFave);
+        $(".gifContainer").append(buttonSave);*/
 
 
     })
