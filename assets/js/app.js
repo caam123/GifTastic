9d7de9006cf018587de5778fbc22dcd4b2bc9dfb
Charlie <caam_1275@hotmail.com>
2019-04-04 23:34:38 -0600

    added displayButtons function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,26 @@
-console.log("hola");
+
+var bands = ["George Michael", "Erasure", "Pet Shop Boys", "OMD", "Madonna"];
+
+function displayButtons(){
+
+    $("#botonesList").empty();
+
+    for (var i = 0; i < bands.length; i++) {
+        
+        var button = $("<button>");
+        button.addClass("botones");
+        button.attr("data-name", bands[i]);
+        button.text(bands[i]);
+        $("#botonesList").append(button);
+
+    }
+};
+
+$()
+
+
+
+
+
+
+displayButtons();
