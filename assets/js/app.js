663cc55c52dc6e1a5c240c0c9355b9d5cb8a66e3
Charlie <caam_1275@hotmail.com>
2019-04-05 00:17:36 -0600

    added function click to get data attr from buttons, keep in mind arguments inside onclick

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 
 var bands = ["George Michael", "Erasure", "Pet Shop Boys", "OMD", "Madonna"];
 
+
+
+// 1. Funcion Display Buttons - Aparecen en la página
 function displayButtons(){
 
     $("#botonesList").empty();
@@ -16,11 +19,34 @@ function displayButtons(){
     }
 };
 
-$()
+
+// 2. Funcion addBand que agrega nuevo boton segun el input > Triggers displayButtons()
+
+$("#addBand").on("click", function(event){
+    event.preventDefault();
+
+    var band = $("#searchQuery").val().trim();
+
+    bands.push(band);
+
+    displayButtons();
+    //Limpiar la barra buscadora
+    $("#searchQuery").val("");
+
+});
+
+// ===== Comentario importante!!! dentro de .on ( ".botones") ayuda a descender del id de botones list y filtrar a través de la clase, por alguna razon no podia hacerlo de la forma de siempre ===== ¿? //
+$("#botonesList").on("click", ".botones", function(){
+    console.log("click");
+
+    var inputBand = $(this).attr("data-name");
+
+});
 
 
 
 
 
+// ===== LLAMADA DE FUNCIONES =====
 
 displayButtons();
